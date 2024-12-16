2bd2c1b542cfc4dcba8a8ddf464f2ce2c63c02ea
Ubuntu <ubuntu@ip-172-31-10-222.ap-northeast-2.compute.internal>
2024-12-16 06:07:54 +0000

    tracking API 작성 완료

--- src/router/tracking/sql.js
+++ src/router/tracking/sql.js
@@ -93,5 +93,22 @@ AND
     idx = $2
 `
 
+const putTrackingImageSQL = 
+`
+UPDATE
+    tracking.list
+SET
+    center = $1,
+    zoom = $2,
+    heading = $3,
+    sharing = $4,
+    color = $5,
+    thickness = $6,
+    background = $7
+WHERE
+    user_idx = $8
+AND
+    idx = $9
 
-module.exports = {createTrackingImgSQL,getMyTrackingImgSQL,getUserTrackingImgSQL,deleteTrackingImgSQL,getTrackingLineSQL}
+`
+module.exports = {createTrackingImgSQL,getMyTrackingImgSQL,getUserTrackingImgSQL,deleteTrackingImgSQL,getTrackingLineSQL,putTrackingImageSQL}
